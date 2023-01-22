e484d845c4800f51ef0dbb6f09bbbf4bd418a8ce
Vyacheslav <slava_94@mail.ru>
2023-01-22 21:02:16 +0300

    Project creation

---
+++ src/main/java/com/example/spring_course_springboot2/SpringCourseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_course_springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseSpringboot2Application.class, args);
+    }
+
+}
